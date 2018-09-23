84d9a25141507c8c71f5263258b9a5c85e3e25c9
aedean <deanomonkey@hotmail.co.uk>
2018-09-23 16:46:46 +0100

    Add html and javascript to add storage

--- test_one.js
+++ test_one.js
@@ -1,1 +1,15 @@
-//testing
+//Local Storage 
+
+//Set local storage method
+
+document.getElementById('submit_button').onclick = function() {
+    if (typeof(Storage) !== "undefined") {
+        var username = document.getElementById('name_box').value;
+        // Store
+        localStorage.setItem("username", username);
+        // Retrieve
+        document.getElementById("name_result").innerHTML = localStorage.getItem("username");
+    } else {
+        document.getElementById("name_result").innerHTML = "Sorry, your browser does not support Web Storage..."
+    }
+};​
