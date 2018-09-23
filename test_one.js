f5867012b77040819e41bfad36c3da9c9669b7da
aedean <deanomonkey@hotmail.co.uk>
2018-09-23 17:32:38 +0100

    Fix JS to output name from local storage

--- test_one.js
+++ test_one.js
@@ -1,15 +1,16 @@
-//Local Storage 
-
-//Set local storage method
-
-document.getElementById('submit_button').onclick = function() {
-    if (typeof(Storage) !== "undefined") {
-        var username = document.getElementById('name_box').value;
-        // Store
-        localStorage.setItem("username", username);
-        // Retrieve
+if (typeof(Storage) !== "undefined") {
+    var username = document.getElementById('name_box').value;
+    
+    if(localStorage.getItem("username") != null) {
         document.getElementById("name_result").innerHTML = localStorage.getItem("username");
     } else {
-        document.getElementById("name_result").innerHTML = "Sorry, your browser does not support Web Storage..."
+        document.getElementById("name_result").innerHTML = "Name not set.";
     }
-};​
+
+    document.getElementById('submit_button').onclick = function() {
+        username = document.getElementById('name_box').value;
+        localStorage.setItem("username", username);
+    };
+} else {
+    document.getElementById("name_result").innerHTML = "Sorry, your browser does not support Web Storage...";
+}
